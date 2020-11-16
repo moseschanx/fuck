51eab33cb39beae4c2bbe8b348b6a24a3963a268
moses-Laptop <moseschanx@gmail.com>
2020-11-16 09:51:46 +0800

    moses added

---
+++ Control_Flow.c
@@ -0,0 +1,12 @@
+// An expression becomes a statement when it is followed by a semicolon which is a statement terminator in the C.
+//e.g: 
+// 	expression : x = 0 
+// 	statement  : x = 0;
+//
+
+// Compound statement , or block : A group of declarations and statements are collected by the braces {} with no semicolon after the right brace.
+//
+//
+//
+
+//This is a git testing line .
