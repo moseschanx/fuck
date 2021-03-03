edbfa97fa15f84fc29f02c0915c70047beb37953
Lab.Host <fuck@fuck.com>
2021-03-04 07:36:40 +0800

    new fucking message here

---
+++ enc-rsa/gen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh 
+
+echo "#generating CA certificate and key"
+openssl req -days 365  -newkey rsa:2048 -keyout ca.key -x509 -nodes -new -out ca.crt 
+dir
+
+echo "#generating certificate and key for server" 
+openssl req -days 365  -newkey rsa:2048	-keyout server.key -nodes -new -out server.csr 
+
+openssl x509 -req -days 365 -in server.csr -CA ca.crt -CAkey ca.key --CAcreateserial -out server.crt
+dir
+
+echo "#generating certificate and key for client"
+openssl req -days 365  -newkey rsa:2048	-keyout client.key -nodes -new -out client.csr 
+
+openssl x509 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key --CAcreateserial -out client.crt
+dir
+
+echo "#generating tls-auth key"
+openvpn --genkey -secret ta.key
+
+echo "#generating the Diffie hellman parameters"
+openssl dhparam -out dh2048.pem 2048
+
+
